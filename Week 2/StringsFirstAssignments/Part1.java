7f97bc9c38e2064bc22b381c17f58b655699f074
Harshit Jaitly <f20170692@goa.bits-pilani.ac.in>
2021-01-26 05:01:55 +0530

    Started Week 2 Problems

---
+++ Week 2/StringsFirstAssignments/Part1.java
@@ -0,0 +1,42 @@
+
+/**
+ * Write a description of Part1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public class Part1 {
+    
+    public String findSimpleGene(String dna){
+        // takes input of a DNA String
+        String result = "" ; //declaring an empty string
+        int startIndex = dna.indexOf("ATG") ;
+        if (startIndex == -1){
+            return "" ;
+        }
+        int stopIndex = dna.indexOf("TAA", startIndex+3) ;
+        if (stopIndex == -1){
+            return "" ;
+        }
+        if ((stopIndex - startIndex)%3 == 0){
+            result = dna.substring(startIndex, stopIndex+3) ;
+        }
+        else{
+            result = "" ;
+        }
+        return result ;
+    }
+    public void testSimpleGene(){
+        String case_1 = "ACGTGCCAATAA" ;
+        String case_2 = "ACGTGCCAAATG" ;
+        String case_3 = "ACGTGCCAATCA" ;
+        String case_4 = "ATGTGCCAATAA" ;
+        String case_5 = "ATGTGCCATAAC" ;
+        System.out.println("DNA = "+case_1+ ", Gene = "+findSimpleGene(case_1));
+        System.out.println("DNA = "+case_2+ ", Gene = "+findSimpleGene(case_2));
+        System.out.println("DNA = "+case_3+ ", Gene = "+findSimpleGene(case_3));
+        System.out.println("DNA = "+case_4+ ", Gene = "+findSimpleGene(case_4));
+        System.out.println("DNA = "+case_5+ ", Gene = "+findSimpleGene(case_5));
+    }
+}
