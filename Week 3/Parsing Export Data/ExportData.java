0d5823f554efe5b58fd22d860fc885bad6cb65c0
Harshit Jaitly <f20170692@goa.bits-pilani.ac.in>
2021-02-07 07:26:22 +0530

    Create Week3_ExportData_Assignment

---
+++ Week 3/Parsing Export Data/ExportData.java
@@ -0,0 +1,58 @@
+/**
+ * Export items and value of different countries
+ * 
+ * Harshit Jaitly 
+ * 7-February-2021
+ */
+
+import edu.duke.* ; 
+import org.apache.commons.csv.* ;
+public class ExportData 
+{
+    public void countryInfo(CSVParser parser, String country)
+    {
+        for(CSVRecord record : parser)
+        {
+            if(record.get("Country").equals(country))
+            {
+                System.out.print(record.get("Country")+": ") ;
+                System.out.print(record.get("Exports")+": ") ;
+                System.out.println(record.get("Value (dollars)")) ;
+                return ;
+            }
+        }
+        System.out.println("NOT FOUND");
+    }
+    public void listExporterTwoProducts(CSVParser parser, String exportitem1 , String exportitem2)
+    {
+        for(CSVRecord record : parser)
+        {
+            String export = record.get("Exports") ;
+            if(export.contains(exportitem1) && export.contains(exportitem2))
+            {
+                System.out.println(record.get("Country")) ;
+            }
+        }
+    }
+    public void numberOfExporters(CSVParser parser, String exportitem)
+    {
+        for(CSVRecord record : parser)
+        {
+            String export = record.get("Exports") ;
+            int exportCount = 0;
+            if(export.contains(exportitem))
+            {
+                exportCount++;
+            }
+        }
+        System.out.println("")
+    }    
+    public void tester()
+    {
+        FileResource fr = new FileResource() ;
+        CSVParser parser = fr.getCSVParser() ;
+        countryInfo(parser, "Germany") ;
+        // parser = fr.getCSVParser() ;
+        listExporterTwoProducts(parser, "gold", "diamonds") ;
+    }
+}
